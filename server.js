3cfea6c27332c444a56a31542538d7c9dcb65b24
GabioMega <gabio.handoko@binus.ac.id>
2025-01-03 17:29:32 +0700

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - test hiii');
+  res.send('Hello World - test hiii world');
 });
 
 app.listen(PORT, HOST, () => {
